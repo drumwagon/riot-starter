bdaaec870243193ec04ea7f33df87447644395c6
Jeff Conner <drumwagon@gmail.com>
2013-12-12 16:50:43 -0600

    Kewl new stuffs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 var items = [
+  { name: 'Apple', price: 1 },
+  { name: 'Banana', price: 2 },
+  { name: 'Carrot', price: 4 },
   { name: 'Ginger', price: 7 },
   { name: 'Pepper', price: 4 },
+  { name: 'Lettuce', price: 2.25 },
   { name: 'Sea Salt', price: 1 },
   { name: 'Dewberry', price: 0.15 },
   { name: 'Cantaloupe', price: 3.50 }
 ];
  
- var itemTemplate = $('#templates .item').html();
+var itemTemplate = $('#templates .item').html();
 
 for (var i = 0; i < items.length; i += 1) {
 var newItemHtml = $.render(itemTemplate, { name: items[i].name, price: items[i].price });
