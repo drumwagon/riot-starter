c24494e15e69dd1e58c2778126975d95ae1fc42b
Jeff Conner <drumwagon@gmail.com>
2013-12-12 16:35:26 -0600

    Kewl new stuffs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert('hi');
+var items = [
+  { name: 'Ginger', price: 7 },
+  { name: 'Pepper', price: 4 },
+  { name: 'Sea Salt', price: 1 },
+  { name: 'Dewberry', price: 0.15 },
+  { name: 'Cantaloupe', price: 3.50 }
+];
+ 
+ var itemTemplate = $('#templates .item').html();
+
+for (var i = 0; i < items.length; i += 1) {
+var newItemHtml = $.render(itemTemplate, { name: items[i].name, price: items[i].price });
+$('#store').append(newItemHtml);
+};
