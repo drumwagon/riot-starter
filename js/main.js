972a053cab400058aedd5023cfa8455e1a5e7937
Jeff Conner <drumwagon@gmail.com>
2013-12-18 13:14:45 -0600

    make music videos work

--- js/main.js
+++ js/main.js
@@ -4,13 +4,17 @@ var videos = [
   { title: 'Sadness', youtubeId: 'sIeJSSjTG3k' }
 ];
 
-var itemTemplate = $('#templates .video-list-item');
+var itemTemplate = $('#templates .video-list-item').html();
 
-var renderVideoList = function () {
-  for (var i = 0; i >= videos.length; i += 1){
-  	var newVideoHtml = $.render(itemTemplate, { title: videos[i].title });
-  	$('#vid-item').append(newVideoHtml);
-	};
+var newVideo = videos.push(newVideo);
+
+for (var i = 0; i < videos.length; i += 1){
+  var video = videos[i];
+  var newVideoHtml = $.render(itemTemplate, { title: video.title, youtubeId: video.youtubeId });
+  $('#vid-item').append(newVideoHtml);
+};
 	// return newVideoHtml;
 console.log(newVideoHtml);
-};
+
+
+
